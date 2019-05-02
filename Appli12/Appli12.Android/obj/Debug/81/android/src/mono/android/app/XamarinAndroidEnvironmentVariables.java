cbc3e09748fe060f283ca94c53cda0dac76375b9
NassimMhb <mhb.nassim@gmail.com>
2019-05-02 12:25:59 +0200

    first commit

---
+++ Appli12/Appli12.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "4f8e413e-e4cb-48f0-8587-0d4f489c91b0",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
---
+++ Appli12/Appli12/NotesPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using Appli12.Models;
+using Xamarin.Forms.Xaml;
+
+namespace Appli12
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotesPage : ContentPage
+	{
+		public NotesPage ()
+		{
+			InitializeComponent ();
+		}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Commercants>();
+
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            foreach (var filename in files)
+            {
+                notes.Add(new Commercants
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                });
+            }
+
+            listView.ItemsSource = notes
+                .OrderBy(d => d.Date)
+                .ToList();
+        }
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPagexaml
+            {
+                BindingContext = new Commercants()
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPagexaml
+                {
+                    BindingContext = e.SelectedItem as Commercants
+                });
+            }
+        }
+    }
+}
